eee645b5ac0f2b8d752e546d7e8a8ae648498510
Carrik McNerlin <carrik.mcnerlin@gmail.com>
2023-10-20 16:45:19 -0400

    Pet separated out, createElements replaced w/ jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom";
-
-const Pet = (props) => {
-    return React.createElement(
-        "div", 
-        {}, 
-        [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-        ]
-    );
-};
+import Pet from "./Pet"
 
 //Always capitalize component names
 const App = () => {
-    return React.createElement(
-        "div", //tag to be created
-        {}, //attributes go in here, or an empty object/null
-        [
-        React.createElement("h1", {}, "Adopt Me!"), //items inside the created tag
-        React.createElement(Pet, {name: "Ezra", animal: "Dog", breed: "Doberman"}),
-        React.createElement(Pet, {name: "Happy", animal: "Cat", breed: "Unknown"}),
-        React.createElement(Pet, {name: "Dutch", animal: "Dog", breed: "Doberman"})
-        ]
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <Pet name="Ezra" animal="Dog" breed="Doberman" />
+            <Pet name="Happy" animal="Cat" breed="Unknown" />
+            <Pet name="Dutch" animal="Dog" breed="Doberman" />
+        </div>
     );
 };
 
 const container = document.getElementById('root'); //grab root element
 const root = ReactDOM.createRoot(container); //point to it with the virtual DOM
 //const root = ReactDOM.render(container, <App />); //This works the same as the above line, but above is preferable for anything React 18+
-root.render(React.createElement(App)); //tell the DOM to render the component called App
+root.render(<App />); //tell the DOM to render the component called App
