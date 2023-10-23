e1d40b696e1fc0a2f0667134a58cae361052a222
Carrik McNerlin <carrik.mcnerlin@gmail.com>
2023-10-22 20:29:58 -0400

    results in own component, pet has img and location

---
+++ src/Results.jsx
@@ -0,0 +1,25 @@
+import Pet from "./Pet";
+
+const Results = ({pets}) => {
+    return (
+        <div className="search">
+            {!pets.length ? 
+            (
+                <h1>No Pets Found</h1>
+            )
+            : (
+                pets.map((pet) => {
+                    return (
+                        <Pet key={pet.id} id={pet.id}
+                            animal={pet.animal} breed={pet.breed}
+                            name={pet.name} images={pet.images}
+                            location={`${pet.city}, ${pet.state}`}
+                        />
+                    );
+                })
+            )}
+        </div>
+    );
+};
+
+export default Results;
